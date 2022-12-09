2a71afe9ec6808b9def574c57a6aebaf5056a56a
hanyun <1355081829@qq.com>
2022-12-09 22:46:26 +0800

    用户列表和删除用户

--- apps/nest-admin/src/admin/user/user.controller.ts
+++ apps/nest-admin/src/admin/user/user.controller.ts
@@ -1,9 +1,6 @@
 import {
   Controller,
   Get,
-  Post,
-  Body,
-  Patch,
   Param,
   Delete,
   Query,
@@ -11,8 +8,7 @@ import {
   ParseIntPipe,
 } from '@nestjs/common';
 import { UserService } from './user.service';
-import { CreateUserDto } from './dto/create-user.dto';
-import { UpdateUserDto } from './dto/update-user.dto';
+
 import { error, paginate, success } from '@app/common';
 
 @Controller('user')
@@ -37,16 +33,16 @@ export class UserController {
     return paginate(users, total);
   }
 
-  @Delete":id"')
-  async remove(@Param"id"', ParseIntPipe) id: number) {
+  @Delete(':id')
+  async remove(@Param('id', ParseIntPipe) id: number) {
     const user = await this.userService.findOne(id);
     if (!user) {
-      return error"用户不存在"');
+      return error('用户不存在');
     }
     const { affected } = await this.userService.remove(+id);
     if (affected > 0) {
       return success();
     }
-    return error"删除失败"');
+    return error('删除失败');
   }
 }
